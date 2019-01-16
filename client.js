924ca4ad8da94e6b072fac5ee111ff6e34746804
Issaret Prachitmutita <imac.monochrome@gmail.com>
2019-01-16 16:23:57 +0700

    Function inital request and addParam

--- client.js
+++ client.js
@@ -7,9 +7,42 @@ function init(url, appId, secretKey) {
   const instance = {}
   instance["URL"] = url ? url : "https://api.lazada.com.my/rest"
   instance["APP_ID"] = appId ? appId : ""
-  instance["SECRET_KEY"] = secretKey ? secretKey: ""
+  instance["SECRET_KEY"] = secretKey ? secretKey : ""
+  return instance
+}
+
+function initRequest(path) {
+  if (path.length < 1) {
+    throw new Error('path is empty!');
+  }
+  const requestInstance = {}
+  requestInstance["PATH"] = path
+  return requestInstance
+}
+
+
+function addApiParam(instance, key, value) {
+  if (!instance || (Object.keys(instance).length === 0 && instance.constructor === Object)) {
+    throw new Error('instance is null');
+  }
+
+  if (!key || key.length<1) {
+    throw new Error('key is null');
+  }
+  if (!value || value.length<1) {
+    throw new Error('value is null');
+  }
+
+  let newInstance = JSON.parse(JSON.stringify(instance));
+  if (!newInstance["params"] || newInstance["params"].length < 1) {
+    newInstance["params"] = []
+  }
+  newInstance["params"].push({key,value})
+  return newInstance
 }
 
 module.exports = {
-  init
+  init,
+  initRequest,
+  addApiParam
 }
--- test/client.test.js
+++ test/client.test.js
@@ -1,14 +1,64 @@
 const CLIENT = require('../client');
 
+expect.extend({
+  toContainObject(received, argument) {
+
+    const pass = this.equals(received,
+      expect.arrayContaining([
+        expect.objectContaining(argument)
+      ])
+    )
+
+    if (pass) {
+      return {
+        message: () => (`expected ${this.utils.printReceived(received)} not to contain object ${this.utils.printExpected(argument)}`),
+        pass: true
+      }
+    } else {
+      return {
+        message: () => (`expected ${this.utils.printReceived(received)} to contain object ${this.utils.printExpected(argument)}`),
+        pass: false
+      }
+    }
+  }
+})
 
 test('InitWithEmptyURL', () => {
   const t = () => {
-    const client = CLIENT.init("","test","test")
+    const client = CLIENT.init("", "test", "test")
   };
   expect(t).toThrow("url is empty!");
 })
 
-test('testInitWithUrlAppIdSecretKeyShouldGotObject',()=> {
-  const client = CLIENT.init("https://api.lazada.com.my/rest","","")
+test('testInitWithUrlAppIdSecretKeyShouldGotObject', () => {
+  const instance = CLIENT.init("https://api.lazada.com.my/rest", "", "")
+  expect(typeof instance).toBe('object');
 })
 
+test('InitRequestWithEmptyURL', () => {
+  const t = () => {
+    const client = CLIENT.initRequest("")
+  };
+  expect(t).toThrow("path is empty!");
+})
+
+test('initRequest', () => {
+  const instance = CLIENT.initRequest("/auth/token/refresh")
+  expect(typeof instance).toBe('object');
+  expect(instance).toEqual( 
+    expect.objectContaining({ 
+      PATH: '/auth/token/refresh' 
+    })
+  )
+})
+
+test('addParam3Param', () => {
+  let instance = CLIENT.initRequest("/auth/token/refresh")
+  instance = CLIENT.addApiParam(instance, "key", "value")
+  instance = CLIENT.addApiParam(instance, "key2", "value2")
+  instance = CLIENT.addApiParam(instance, "key3", "value3")
+  expect(instance.params).toContainObject({"key": "key", "value": "value"})
+  expect(instance.params).toContainObject({"key": "key2", "value": "value2"})
+  expect(instance.params).toContainObject({"key": "key3", "value": "value3"})
+
+})
